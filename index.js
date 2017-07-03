33769db894f71528e869724d8f85d02834fc0c9a
crossgit <xhken000@163.com>
2017-07-03 17:39:06 +0800

    调试

---
+++ index.js
@@ -0,0 +1,15 @@
+import xyBtnStyle from './xyview/button/index.vue';
+
+const install = function (Vue) {
+    if (install.installed) return; 
+    Vue.component(xyBtnStyle.name, xyBtnStyle); 
+};
+
+if (typeof window !== 'undefined' && window.Vue) {
+    install(window.Vue);
+};
+
+export {
+    install, 
+    xyBtnStyle
+};
