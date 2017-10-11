1d17aa153d7f3bc82b8745def906bb1eced639c5
crossgit <xhken000@163.com>
2017-10-11 17:09:30 +0800

    修改preview在safari下的问题

--- src/components/preview.vue
+++ src/components/preview.vue
@@ -16,23 +16,28 @@
 // title 图片下方显示的介绍说明,可为空
 export default {
     data() {
-        return { 
+        return {
             imgs: [
                 {
-                    src: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1494484569&di=ca0f556611089513191251ceab4c3217&imgtype=jpg&er=1&src=http%3A%2F%2Fwww.xiaomayi88.com%2Fuploads%2Fimg%2F140411%2F97-14041113555Q96.jpg',
-                    title: '百度图片'
+                    src: require('../assets/logo.png'),
+                    title: '什么'
                 },
                 {
-                    src: 'https://placekitten.com/800/400',
-                    title: '这里写一段话'
+                    src: require('../assets/p1.jpg'),
+                    title: '什么'
                 },
                 {
-                    src: 'https://placekitten.com/1200/900',
-                }],
+                    src: require('../assets/p2.jpg'),
+                    title: '什么'
+                },
+                {
+                    src: require('../assets/p3.jpg'),
+                    title: '什么'
+                },
+            ],
             list: null,
         }
     },
-
     methods: {
         show(index) {
             let list = [
@@ -47,9 +52,29 @@ export default {
                 {
                     src: 'https://placekitten.com/1200/900',
                 }];
+            // 转64位,看情况
+            // this.imgs.map(v => {
+            //     this.convertImgToBase64(v.src, str64 => v.src = str64, 'image/jpg')
+            // })
             // 固定写法
-            this.$refs.previewer.show(index, document.querySelectorAll('.previewer-img'), list);
+            this.$refs.previewer.show(index, document.querySelectorAll('.previewer-img'), this.imgs);
 
+        },
+
+        convertImgToBase64(url, callback, outputFormat) {
+            var canvas = document.createElement('CANVAS'),
+                ctx = canvas.getContext('2d'),
+                img = new Image;
+            img.crossOrigin = 'Anonymous';
+            img.onload = function() {
+                canvas.height = img.height;
+                canvas.width = img.width;
+                ctx.drawImage(img, 0, 0);
+                var dataURL = canvas.toDataURL(outputFormat || 'image/png');
+                callback.call(this, dataURL);
+                canvas = null;
+            };
+            img.src = url;
         }
     }
 }
--- xyview/preview/index.vue
+++ xyview/preview/index.vue
@@ -5,40 +5,40 @@
 <template>
     <!-- Root element of PhotoSwipe. Must have class pswp. vux-previewer-->
     <div class="pswp" tabindex="-1" role="dialog" aria-hidden="true" style="z-index:1500">
-    
+
         <!-- Background of PhotoSwipe. 
-                     It's a separate element as animating opacity is faster than rgba(). -->
+                                                                                                                                             It's a separate element as animating opacity is faster than rgba(). -->
         <div class="pswp__bg"></div>
-    
+
         <!-- Slides wrapper with overflow:hidden. -->
         <div class="pswp__scroll-wrap">
-    
+
             <!-- Container that holds slides. 
-                        PhotoSwipe keeps only 3 of them in the DOM to save memory.
-                        Don't modify these 3 pswp__item elements, data is added later on. -->
+                                                                                                                                                PhotoSwipe keeps only 3 of them in the DOM to save memory.
+                                                                                                                                                Don't modify these 3 pswp__item elements, data is added later on. -->
             <div class="pswp__container">
                 <div class="pswp__item"></div>
                 <div class="pswp__item"></div>
                 <div class="pswp__item"></div>
             </div>
-    
+
             <!-- Default (PhotoSwipeUI_Default) interface on top of sliding area. Can be changed. -->
             <div class="pswp__ui pswp__ui--hidden">
-    
+
                 <div class="pswp__top-bar">
-    
+
                     <!--  Controls are self-explanatory. Order can be changed. -->
-    
+
                     <div class="pswp__counter"></div>
-    
+
                     <button class="pswp__button pswp__button--close" title="Close (Esc)"></button>
-    
+
                     <button class="pswp__button pswp__button--share" title="Share"></button>
-    
+
                     <button class="pswp__button pswp__button--fs" title="Toggle fullscreen"></button>
-    
+
                     <button class="pswp__button pswp__button--zoom" title="Zoom in/out"></button>
-    
+
                     <!-- Preloader demo http://codepen.io/dimsemenov/pen/yyBWoR -->
                     <!-- element will get class pswp__preloader--active when preloader is running -->
                     <div class="pswp__preloader">
@@ -49,25 +49,25 @@
                         </div>
                     </div>
                 </div>
-    
+
                 <div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
                     <div class="pswp__share-tooltip"></div>
                 </div>
-    
+
                 <button class="pswp__button pswp__button--arrow--left" title="Previous (arrow left)">
                 </button>
-    
+
                 <button class="pswp__button pswp__button--arrow--right" title="Next (arrow right)">
                 </button>
-    
+
                 <div class="pswp__caption">
                     <div class="pswp__caption__center"></div>
                 </div>
-    
+
             </div>
-    
+
         </div>
-    
+
     </div>
 </template>
 
@@ -78,7 +78,7 @@ import UI from 'photoswipe/dist/photoswipe-ui-default'
 export default {
     name: 'xyPreview',
     methods: {
-        init(index, dom, list) {
+        init(index, dom, list, cb) {
             let self = this;
             let options = Object.assign({
                 history: false,
@@ -86,7 +86,7 @@ export default {
                 tapToClose: true,
                 fullscreenEl: false,
                 index: index,
-                errorMsg:'<div class="pswp__error-msg"> 图片加载失败.</div>',
+                errorMsg: '<div class="pswp__error-msg"> 图片加载失败.</div>',
                 getThumbBoundsFn(index) {
                     // find thumbnail element
                     // let thumbnail = document.querySelectorAll('.previewer-demo-img')[index];  
@@ -102,26 +102,45 @@ export default {
                     // http://javascript.info/tutorial/coordinates
                 }
             }, this.options);
-            list.forEach(function (item) {
-                let img = new Image();
-                img.src = item.src;
-                item.w = img.width;
-                item.h = img.height;
-            })
-            this.photoswipe = new PhotoSwipe(this.$el, UI, list, options)
-            this.photoswipe.init()
-            this.photoswipe.listen('close', () => {
-                this.$emit('on-close')
-            })
+            let p = 0; 
+            new Promise(function(resolve) {
+                for (let item of list) {
+                    // list.forEach(function (item) {
+                    let img = new Image();
+                    img.src = item.src;
+                    let timer = setInterval(function() {
+                        if (img.complete) {
+                            clearInterval(timer); 
+                            item.w = img.width;
+                            item.h = img.height; 
+                            p++; 
+                            if (p == list.length)
+                                resolve(p);
+                        }
+                    }, 10);
+                }  
+            }).then(function(p) {
+                self.photoswipe = new PhotoSwipe(self.$el, UI, list, options)
+                self.photoswipe.init()
+                self.photoswipe.listen('close', () => {
+                    self.$emit('on-close')
+                })
+            }) 
+           
+            //     this.photoswipe = new PhotoSwipe(this.$el, UI, list, options)
+            //     this.photoswipe.init()
+            //     this.photoswipe.listen('close', () => {
+            //         this.$emit('on-close')
+            //     }) 
         },
         show(index, dom, list) {
-            this.init(index, dom, list)
+            this.init(index, dom, list);
         },
         close() {
             this.photoswipe.close()
         }
     },
-    props: { 
+    props: {
         index: {
             type: Number,
             default: 0
